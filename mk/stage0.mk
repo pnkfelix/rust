2afd00c01372478a1ee55526d80170de409b9976
Niko Matsakis <niko@alum.mit.edu>
2011-12-24 14:15:15 -0800

    start Makefile edits

--- mk/stage0.mk
+++ mk/stage0.mk
@@ -1,11 +1,23 @@
 # Extract the snapshot host compiler
 
-$(HBIN0_H_$(CFG_HOST_TRIPLE))/rustc$(X):		\
-		$(S)src/snapshots.txt					\
+ifndef CFG_SNAPSHOT
+
+# By default: find the snapshot 
+$(HBIN0_H_$(CFG_HOST_TRIPLE))/rustc$(X):			\
+		$(S)src/snapshots.txt				\
 		$(S)src/etc/get-snapshot.py $(MKFILE_DEPS)
 	@$(call E, fetch: $@)
 	$(Q)$(S)src/etc/get-snapshot.py $(CFG_HOST_TRIPLE)
 	$(Q)touch $@
+
+else 
+
+$(HBIN0_H_$(CFG_HOST_TRIPLE))/rustc$(X):
+	@$(call E, fetch: $@)
+	$(Q)$(S)src/etc/get-snapshot.py $(CFG_HOST_TRIPLE)
+	$(Q)touch $@
+
+endif
 
 # Host libs will be extracted by the above rule
 
@@ -36,9 +48,9 @@ define BOOTSTRAP_STAGE0
   # $(2) stage to bootstrap from
   # $(3) target to bootstrap from
 
-$$(HBIN0_H_$(1))/rustc$$(X):								\
-		$$(TBIN$(2)_T_$(1)_H_$(3))/rustc$$(X)
-	@$$(call E, cp: $$@)
+$$(HBIN0_H_$(1))/rustc$$(X):				\
+		$$(TBIN$(2)_T_$(1)_H_$(3))/rustc$$(X)	\
+	@$$(call E, cp: $$@)				\
 	$$(Q)cp $$< $$@
 
 $$(HLIB0_H_$(1))/$$(CFG_RUNTIME): \
