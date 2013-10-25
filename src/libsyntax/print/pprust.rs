0194014a5eacb55015babb17c1d2c5ef2e53d978
Felix S. Klock II <pnkfelix@pnkfx.org>
2013-10-26 00:29:27 +0200

    Compare identifiers non-hygenienically in pretty-printer to bypass internal assertion.

--- src/libsyntax/print/pprust.rs
+++ src/libsyntax/print/pprust.rs
@@ -1857,7 +1857,7 @@ pub fn print_meta_item(s: @ps, item: &ast::MetaItem) {
 pub fn print_view_path(s: @ps, vp: &ast::view_path) {
     match vp.node {
       ast::view_path_simple(ident, ref path, _) => {
-        if path.segments.last().identifier != ident {
+        if path.segments.last().identifier.name != ident.name {
             print_ident(s, ident);
             space(s.s);
             word_space(s, "=");
