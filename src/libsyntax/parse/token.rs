98c5bfe16131f6cdcab634671005ee115b3d6399
Felix S. Klock II <pnkfelix@pnkfx.org>
2013-11-11 10:56:38 +0100

    Add `unsized` keyword for DST prototyping.

--- src/libsyntax/parse/token.rs
+++ src/libsyntax/parse/token.rs
@@ -489,14 +489,15 @@ fn mk_fresh_ident_interner() -> @ident_interner {
         "in",                 // 63
         "continue",           // 64
         "proc",               // 65
+        "unsized",            // 66
 
-        "be",                 // 66
-        "pure",               // 67
-        "yield",              // 68
-        "typeof",             // 69
-        "alignof",            // 70
-        "offsetof",           // 71
-        "sizeof",             // 72
+        "be",                 // 67
+        "pure",               // 68
+        "yield",              // 69
+        "typeof",             // 70
+        "alignof",            // 71
+        "offsetof",           // 72
+        "sizeof",             // 73
     ];
 
     @interner::StrInterner::prefill(init_vec)
@@ -505,9 +506,9 @@ fn mk_fresh_ident_interner() -> @ident_interner {
 static SELF_KEYWORD_NAME: uint = 8;
 static STATIC_KEYWORD_NAME: uint = 27;
 static STRICT_KEYWORD_START: uint = 32;
-static STRICT_KEYWORD_FINAL: uint = 65;
-static RESERVED_KEYWORD_START: uint = 66;
-static RESERVED_KEYWORD_FINAL: uint = 72;
+static STRICT_KEYWORD_FINAL: uint = 66;
+static RESERVED_KEYWORD_START: uint = 67;
+static RESERVED_KEYWORD_FINAL: uint = 73;
 
 // if an interner exists in TLS, return it. Otherwise, prepare a
 // fresh one.
@@ -657,10 +658,15 @@ pub mod keywords {
         Pure,
         Sizeof,
         Typeof,
+        Unsized,
         Yield,
     }
 
     impl Keyword {
+        // The indices here must correspond to the numbers in
+        // special_idents, in mk_fresh_ident_interner, and in static
+        // constants in this file.
+
         pub fn to_ident(&self) -> Ident {
             match *self {
                 As => Ident { name: 32, ctxt: 0 },
@@ -699,14 +705,15 @@ pub mod keywords {
                 While => Ident { name: 62, ctxt: 0 },
                 Continue => Ident { name: 64, ctxt: 0 },
                 Proc => Ident { name: 65, ctxt: 0 },
+                Unsized => Ident { name: 66, ctxt: 0 },
 
-                Alignof => Ident { name: 70, ctxt: 0 },
-                Be => Ident { name: 66, ctxt: 0 },
-                Offsetof => Ident { name: 71, ctxt: 0 },
-                Pure => Ident { name: 67, ctxt: 0 },
-                Sizeof => Ident { name: 72, ctxt: 0 },
-                Typeof => Ident { name: 69, ctxt: 0 },
-                Yield => Ident { name: 68, ctxt: 0 },
+                Alignof => Ident { name: 71, ctxt: 0 },
+                Be => Ident { name: 67, ctxt: 0 },
+                Offsetof => Ident { name: 72, ctxt: 0 },
+                Pure => Ident { name: 68, ctxt: 0 },
+                Sizeof => Ident { name: 73, ctxt: 0 },
+                Typeof => Ident { name: 70, ctxt: 0 },
+                Yield => Ident { name: 69, ctxt: 0 },
             }
         }
     }
