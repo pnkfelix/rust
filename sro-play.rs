553867ec93fce9420b91091140c7bb20bb1c24f4
Felix S. Klock II <pnkfelix@pnkfx.org>
2013-11-18 19:46:13 +0100

    continued playing with GC API.

--- sro-play.rs
+++ sro-play.rs
@@ -1,6 +1,8 @@
+#[allow(unused_imports)];
 use std::cast;
 use std::libc;
 use std::local_data;
+use std::rt::dump_registers;
 
 type DumpedRegsArea = [u64, ..80];
 
@@ -9,32 +11,52 @@ struct ctxt {
     info: ()
 }
 
+impl Clone for ctxt {
+    fn clone(&self) -> ctxt { ctxt { id: self.id + 1, info: () } }
+}
+
 impl Drop for ctxt {
     fn drop(&mut self) {
         println!("dropping ctxt, id: {}", self.id);
     }
 }
 
-fn walk_managed(c: ~ctxt) {
+fn walk_managed(ctxt: ~ctxt) {
     // Dumping xmm state requires 16-byte alignement (at least when
     // using `movapd`; not yet sure about `fxsave`).
-    let regs = [0u64, ..80];
-    let r : uint = cast::transmute(&regs);
-    let r = (r + 16) & (!15); // round-up to 16-byte alignment.
-    let action : *libc::c_void = cast::transmute(do_walk_managed);
-    let c : *libc::c_void = cast::transmute(c);
-    dump_registers(cast::transmute(r), action, c);
+    unsafe {
+        let regs = [0u64, ..80];
+        let r : uint = cast::transmute(&regs);
+        let r = (r + 16) & (!15); // round-up to 16-byte alignment.
+        let action : *libc::c_void = cast::transmute(do_walk_managed);
+        let ctxt_cloned = ctxt.clone();
+        let c : *libc::c_void = cast::transmute(ctxt);
+        let a1 = cast::transmute(r);
+        let a2 = action;
+        let a3 = c;
+        println!("about to dump_registers(0x{:x}, 0x{:x}, 0x{:x}), ctxt: {:?} at 0x{:x}",
+                 a1 as libc::c_int, a2, a3, ctxt_cloned, c as libc::c_int);
+        dump_registers(a1, a2, a3);
+    }
 }
 
-fn do_walk_managed(c: ~ctxt) {
-    println!("got to do_walk_managed, ctxt: {:?}", c);
+extern "C" fn do_walk_managed(_regs: *libc::c_void, _action: *libc::c_void, ctxt_ptr: *libc::c_void) {
+    println!("do_walk_managed(0x{:x}, 0x{:x}, 0x{:x})",
+             _regs as libc::c_int,
+             _action as libc::c_int,
+             ctxt_ptr as libc::c_int);
+    let c : *libc::c_void;
+    let ctxt_cloned;
+    unsafe {
+        let ctxt: ~ctxt = cast::transmute(ctxt_ptr);
+        ctxt_cloned = ctxt.clone();
+        c = cast::transmute(ctxt);
+    }
+    println!("got to do_walk_managed, ctxt: {:?} at 0x{:x}", ctxt_cloned, c as libc::c_int);
 }
 
 fn main() {
     println!("Hello world.");
-    unsafe {
-        do local_data::each_retained_ptr |cv| {
-            println!("{:?}", cv);
-        }
-    }
+    let ctxt = ~ctxt { id: 100, info: () };
+    walk_managed(ctxt);
 }
