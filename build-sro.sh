8d5fad637b5e285c52febcf404b77a8aa81768b1
Felix S. Klock II <pnkfelix@pnkfx.org>
2013-11-20 15:32:52 +0100

    Added debug flags during debugging of sro-play.rs.
    Not sure they actually buy me anything here (they did not seem to help
    in tracking down the `dump_registers` overwrite issue).

--- build-sro.sh
+++ build-sro.sh
@@ -1,4 +1,6 @@
 set -e
 
 ( cd ./objdir-dbgopt && make )
-./objdir-dbgopt/x86_64-apple-darwin/stage2/bin/rustc sro-play.rs
+./objdir-dbgopt/x86_64-apple-darwin/stage2/bin/rustc \
+    -Z debug-info -Z extra-debug-info                \
+    sro-play.rs
