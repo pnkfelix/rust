b743f47b75847d5ba4469d6cf9982468ea772323
Felix S. Klock II <pnkfelix@pnkfx.org>
2013-11-20 10:23:06 +0100

    Still exploring register walking code.
    Still not clear on what I'm doing wrong but OS X's malloc/free is
    complaining at me about modifies-post-free.

--- sro-play.rs
+++ sro-play.rs
@@ -2,7 +2,7 @@
 use std::cast;
 use std::libc;
 use std::local_data;
-use std::rt::DumpedRegs;
+use std::rt::{DumpedRegs,Registers};
 
 type DumpedRegsArea = [u64, ..80];
 
@@ -21,50 +21,63 @@ impl Drop for ctxt {
     }
 }
 
-fn walk_managed(ctxt: ~ctxt) {
+fn walk_managed(mut ctxt: ~ctxt) {
     // Dumping xmm state requires 16-byte alignement (at least when
     // using `movapd`; not yet sure about `fxsave`).
-    unsafe {
-        let regs = [0u64, ..80];
-        let r : uint = cast::transmute(&regs);
-        let r = (r + 16) & (!15); // round-up to 16-byte alignment.
-        let action = do_walk_managed;
-        let ctxt_cloned = ctxt.clone();
-        let c : *libc::c_void = cast::transmute(ctxt);
-        let mut regs : DumpedRegs = cast::transmute(r);
-        let a1 : int = cast::transmute(regs);
-        let a2 = action as libc::c_int;
-        let a3 = c as libc::c_int;
-        println!("about to dump_registers(regs: 0x{:x}, \
-                                          action: 0x{:x}, \
-                                          ctxt: 0x{:x}), \
-                  ctxt: {:?} at 0x{:x}",
-                 a1, a2, a3, ctxt_cloned, a3);
-        let mut ctxt : ~ctxt = cast::transmute(c);
-        regs = cast::transmute(a1);
-        regs.dump(ctxt, action);
-    }
+    let action = do_walk_managed;
+
+    // let regs = [0u64, ..80];
+    // let r : uint = cast::transmute(&regs);
+    // let r = (r + 16) & (!15); // round-up to 16-byte alignment.
+
+    // let mut regs : DumpedRegs = cast::transmute(r);
+    let mut regs : DumpedRegs = DumpedRegs::new_unfilled();
+
+    let c : int;
+    let a1 : int;
+    unsafe { c = cast::transmute(ctxt); ctxt = cast::transmute(c); }
+    unsafe { a1 = cast::transmute(regs); regs = cast::transmute(a1); }
+
+    let a2 = action as int;
+    let a3 = c;
+
+    println!("about to regs.dump (regs: 0x{:x}, \
+                                  action: 0x{:x}, \
+                                  ctxt: 0x{:x}), \
+              regs: {:?} ctxt: {:?} at 0x{:x}",
+             a1, a2, a3, regs, ctxt, a3);
+    regs.dump(&mut *ctxt, action);
+    println!("post the regs.dump (regs: 0x{:x}, \
+                                  action: 0x{:x}, \
+                                  ctxt: 0x{:x}), \
+              regs: {:?} ctxt: {:?} at 0x{:x}",
+             a1, a2, a3, regs, ctxt, a3);
 }
 
-extern "C" fn do_walk_managed(regs: &mut DumpedRegs, mut ctxt: &mut ctxt) {
+extern "C" fn do_walk_managed(mut regs: &mut Registers, mut ctxt: &mut ctxt) {
+    println!("do_walk_managed");
+    let a1 : int;
+    let ctxt_ptr : int;
     unsafe {
-        println!("do_walk_managed");
-        let a1 : int = cast::transmute(regs);
-        let ctxt_ptr : *() = cast::transmute(ctxt);
-        println!("do_walk_managed(regs: 0x{:x}, ctxt: 0x{:x})",
-                 a1.clone() as libc::c_int,
-                 ctxt_ptr as libc::c_int);
-        regs = cast::transmute(a1);
+        a1 = cast::transmute(regs);
+        ctxt_ptr = cast::transmute(ctxt);
+
+        // Apparently it is super-important that I restore the
+        // transmutes before allowing any allocation (incl. ~-), such
+        // as that performed by format! within println!.
+        //
+        // Or maybe error I am seeing is just non-deterministic.  Grr.
         ctxt = cast::transmute(ctxt_ptr);
+        regs = cast::transmute(a1);
+        println!("do_walk_managed(regs: 0x{:x}, ctxt: 0x{:x})", a1, ctxt_ptr);
     }
-    let c : *libc::c_void;
-    let ctxt_cloned;
+    let c : int;
     unsafe {
-        ctxt_cloned = ctxt.clone();
         c = cast::transmute(ctxt);
+        ctxt = cast::transmute(c);
     }
     println!("got to do_walk_managed, regs: {:?} ctxt: {:?} at 0x{:x}",
-             regs, ctxt_cloned, c as libc::c_int);
+             regs, ctxt, c as libc::c_int);
 }
 
 fn main() {
