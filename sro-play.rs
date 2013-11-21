b11daac40b0e94f1a3ef27d2ac740602f3c7c766
Felix S. Klock II <pnkfelix@pnkfx.org>
2013-11-21 20:07:37 +0100

    Further play and tracked down some oddities with -Z extra-debug-info.

--- sro-play.rs
+++ sro-play.rs
@@ -1,18 +1,29 @@
+#[feature(managed_boxes)];
+
 #[allow(unused_imports)];
 use std::cast;
 use std::libc;
 use std::local_data;
+use std::ptr;
+use std::rt::local_heap;
+use std::rt::local_heap::{MemoryRegion, Box};
 use std::rt::{DumpedRegs,Registers};
 
 type DumpedRegsArea = [u64, ..80];
 
 struct ctxt {
+    verbose: bool,
     id: int,
     info: ()
 }
 
 impl Clone for ctxt {
-    fn clone(&self) -> ctxt { ctxt { id: self.id + 1, info: () } }
+    fn clone(&self) -> ctxt {
+        if self.verbose {
+            println!("cloning ctxt id: {}", self.id);
+        }
+        ctxt { id: self.id + 1, ..*self }
+    }
 }
 
 impl Drop for ctxt {
@@ -50,20 +61,69 @@ fn walk_managed(mut ctxt: ~ctxt) {
     let a2 = action as int;
     let a3 = c;
 
-    println!("about to regs.dump (regs: 0x{:x} \\{ .regs: 0x{:x} \\}, \
-                                  action: 0x{:x}, \
-                                  ctxt: 0x{:x}), \
-              regs: {:?} ctxt: {:?} at 0x{:x}",
-             a1, ir, a2, a3, regs, ctxt, a3);
+    if ctxt.verbose {
+        println!("about to regs.dump (regs: 0x{:x} \\{ .regs: 0x{:x} \\}, \
+                                      action: 0x{:x}, \
+                                      ctxt: 0x{:x}), \
+                  regs: {:?} ctxt: {:?} at 0x{:x}",
+                 a1, ir, a2, a3, regs, ctxt, a3);
+    } else {
+        println!("about to regs.dump");
+    }
     regs.dump(&mut *ctxt, action);
-    println!("post the regs.dump (regs: 0x{:x}, \
-                                  action: 0x{:x}, \
-                                  ctxt: 0x{:x}), \
-              regs: {:?} ctxt: {:?} at 0x{:x}",
-             a1, a2, a3, regs, ctxt, a3);
+    if ctxt.verbose {
+        println!("post the regs.dump (regs: 0x{:x}, \
+                                      action: 0x{:x}, \
+                                      ctxt: 0x{:x}), \
+                  regs: {:?} ctxt: {:?} at 0x{:x}",
+                 a1, a2, a3, regs, ctxt, a3);
+    } else {
+        println!("post the regs.dump");
+    }
+}
+
+fn subroutine_alloc() {
+    // enum Ll { Cons(int, @Ll), Nil }
+    // fn alloc_some() -> ~Ll { ~Cons(1, @Cons(2, @Cons(3, @Nil))) }
+    // let mut x = alloc_some();
+    // println!("allocated x: {:?}", x);
+    // *x = Nil;
+
+    let mut x = ~Some(@3);
+
+    println!("allocated x: {:?}", x);
+
+    traverse_live_allocs();
+
+    *x = None;
+
+    println!("overwrote x: {:?}", x);
+
+    traverse_live_allocs();
+}
+
+fn traverse_live_allocs() {
+    let bp : *mut Box = local_heap::live_allocs();
+    println!("bp: {:?}", bp);
+    let mut cursor = bp;
+    let mut count = 0;
+    if cursor != ptr::mut_null() {
+        loop {
+            unsafe {
+                println!("{: >5d}: *cursor: {:?}", count, *cursor);
+                cursor = (*cursor).next;
+            }
+            count += 1;
+            if cursor == ptr::mut_null() { break; }
+            if cursor == bp { fail!("uh oh"); }
+        }
+    }
 }
 
 extern "C" fn do_walk(mut regs: &mut Registers, mut ctxt: &mut ctxt, _:*()) {
+    use std::rt::task::Task;
+    use std::rt::local::Local;
+
     println!("do_walk");
     let a1 : int;
     let ctxt_ptr : int;
@@ -80,17 +140,44 @@ extern "C" fn do_walk(mut regs: &mut Registers, mut ctxt: &mut ctxt, _:*()) {
         regs = cast::transmute(a1);
         println!("do_walk(regs: 0x{:x}, ctxt: 0x{:x})", a1, ctxt_ptr);
     }
+
+    println!("about to enter subroutine");
+
+    subroutine_alloc();
+
+    println!("completed with subroutine");
+
+    traverse_live_allocs();
+
     let c : int;
     unsafe {
         c = cast::transmute(ctxt);
         ctxt = cast::transmute(c);
     }
-    println!("got to do_walk, regs: {:?} ctxt: {:?} at 0x{:x}",
-             regs, ctxt, c as libc::c_int);
+    if ctxt.verbose {
+        println!("got to do_walk, regs: {:?} ctxt: {:?} at 0x{:x}",
+                 regs, ctxt, c as libc::c_int);
+    } else {
+        println!("got to do_walk");
+    }
+
+    fn traverse_local_heap_structure() {
+        Local::borrow(|task: &mut Task| {
+
+                struct MyLocalHeap { // XXX keep synced w/ local_heap::LocalHeap
+                    memory_region: MemoryRegion,
+                    poison_on_free: bool,
+                    live_allocs: *mut Box,
+                }
+
+                let hp : &MyLocalHeap = unsafe { cast::transmute(&task.heap) };
+                println!("hp: {:?}", hp);
+            });
+    }
 }
 
 fn main() {
     println!("Hello world.");
-    let ctxt = ~ctxt { id: 100, info: () };
+    let ctxt = ~ctxt { verbose: false, id: 100, info: () };
     walk_managed(ctxt);
 }
