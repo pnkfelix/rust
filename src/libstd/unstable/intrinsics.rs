99c977122baf3b388d4f54bd530afe25dc972751
Felix S. Klock II <pnkfelix@pnkfx.org>
2013-11-25 17:38:38 +0100

    Added `is_sendable<T>()` intrinstic for introspection on T.
    This is analogous to the `owns_managed<T>` intrinsic.  The goal in
    this case is to be able to discriminate between ~T and ~[T] for T:Send
    versus !T:Send, in order to maintain metadata for conservative
    scanning (which AFAICT will need to extend outward from the
    roots+stack to owned data in the exchange heap).
    
    Hopefully we will eventually be able to do precise stack scanning, at
    which time discriminating in this manner won't be necessary (or at
    least we will not need to build up or at least use the metadata here).
    But for a conservative stack scan, we need some way to track interior
    pointers into, and the extents of, ~owned memory on the exchange heap.

--- src/libstd/unstable/intrinsics.rs
+++ src/libstd/unstable/intrinsics.rs
@@ -335,6 +335,10 @@ extern "rust-intrinsic" {
     /// Returns `true` if a type is managed (will be allocated on the local heap)
     pub fn owns_managed<T>() -> bool;
 
+    /// Returns `true` if a type implements Send (can be transferred between tasks).
+    #[cfg(not(stage0))]
+    pub fn is_sendable<T>() -> bool;
+
     pub fn visit_tydesc(td: *TyDesc, tv: &mut TyVisitor);
 
     /// Get the address of the `__morestack` stack growth function.
