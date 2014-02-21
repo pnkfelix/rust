7a0b4a44a169975dfb9a5f211fbb6e0e813e9c71
Felix S. Klock II <pnkfelix@pnkfx.org>
2014-02-21 12:47:43 +0100

    Fixed expr cxn to do the id numbering pass before the cfg pass.
    Factored syntax construction code out into separate file.

---
+++ cfg/easy_syntax.rs
@@ -0,0 +1,114 @@
+extern mod getopts;
+extern mod syntax;
+extern mod rustc;
+
+use std::cell::RefCell;
+use std::hashmap::HashMap;
+use diag = syntax::diagnostic;
+use syntax::ast;
+use syntax::ast_map;
+use syntax::codemap;
+use syntax::parse;
+use syntax::parse::token;
+use syntax::parse::token::IdentInterner;
+use syntax::print::pprust;
+use rustc::driver::session;
+use rustc::driver::driver;
+use rustc::middle::lang_items::LanguageItems;
+use rustc::middle::region;
+use rustc::middle::ty;
+
+struct AddNodeIds {
+    sess: session::Session
+}
+
+impl AddNodeIds {
+    fn new(sess: session::Session) -> AddNodeIds { AddNodeIds { sess: sess } }
+}
+
+impl ast_map::FoldOps for AddNodeIds {
+    fn new_id(&self, _old_id: ast::NodeId) -> ast::NodeId {
+        let i = self.sess.next_node_id();
+        println!("new_id: {}", i);
+        i
+    }
+}
+
+pub fn mk_context() -> (AddNodeIds, @diag::SpanHandler, ty::ctxt) {
+    fn hm<K:Eq+IterBytes,V>() -> HashMap<K,V> { HashMap::new() }
+    fn ref_hm<K:Eq+IterBytes+'static,V:'static>() -> @RefCell<HashMap<K,V>> {
+        @RefCell::new(hm())
+    }
+
+    let matches = getopts::getopts(&[], driver::optgroups());
+    let matches = matches.ok().unwrap();
+    let sessopts = driver::build_session_options(&matches);
+    let sess = driver::build_session(sessopts, None);
+    let dm                  = ref_hm();
+    let diag                = diag::mk_span_handler(diag::mk_handler(),
+                                                    @codemap::CodeMap::new());
+    let crate_ = ast::Crate {
+            module: ast::Mod {view_items: ~[], items: ~[]},
+            attrs: ~[],
+            config: ~[],
+            span: codemap::Span {
+                lo: codemap::BytePos(10),
+                hi: codemap::BytePos(20),
+                expn_info: None,
+            },
+    };
+    let add_node_ids        = AddNodeIds::new(sess);
+    let (crate_, amap)      = ast_map::map_crate(diag, crate_, add_node_ids);
+    let freevars            = hm();
+    let named_region_map    = ref_hm();
+    let region_paramd_items = region::resolve_crate(sess, &crate_);
+    let lang_items          = @LanguageItems::new();
+
+    let tcx = ty::mk_ctxt(sess, dm, named_region_map, amap, freevars,
+                          region_paramd_items, lang_items);
+    (add_node_ids, diag, tcx)
+}
+
+pub trait SyntaxToStr {
+    fn get_to_str() -> fn (_: &Self, intr: @IdentInterner) -> ~str;
+
+    fn get_interner(&self) -> @IdentInterner {
+        token::get_ident_interner()
+    }
+    fn to_str(&self) -> ~str {
+        SyntaxToStr::get_to_str()(self, self.get_interner())
+    }
+}
+
+macro_rules! impl_stx_to_str {
+    ($Type:path, $func:path) => {
+        impl SyntaxToStr for $Type {
+            fn get_to_str() -> fn (_: &$Type, intr: @IdentInterner) -> ~str {
+
+                $func
+            }
+        }
+    }
+}
+
+impl_stx_to_str!(ast::Ty,       pprust::ty_to_str)
+impl_stx_to_str!(ast::Pat,      pprust::pat_to_str)
+impl_stx_to_str!(ast::Expr,     pprust::expr_to_str)
+impl_stx_to_str!(ast::Stmt,     pprust::stmt_to_str)
+impl_stx_to_str!(ast::Item,     pprust::item_to_str)
+impl_stx_to_str!(ast::Generics, pprust::generics_to_str)
+impl_stx_to_str!(ast::Path,     pprust::path_to_str)
+
+pub trait QuoteCtxt {
+    fn parse_sess(&self) -> @parse::ParseSess;
+    fn cfg(&self) -> ast::CrateConfig;
+    fn call_site(&self) -> codemap::Span;
+    fn ident_of(&self, st: &str) -> ast::Ident;
+}
+
+impl QuoteCtxt for () {
+    fn parse_sess(&self) -> @parse::ParseSess  { parse::new_parse_sess() }
+    fn cfg(&self) -> ast::CrateConfig          { ~[] }
+    fn call_site(&self) -> codemap::Span       { codemap::DUMMY_SP }
+    fn ident_of(&self, st: &str) -> ast::Ident { token::str_to_ident(st) }
+}
