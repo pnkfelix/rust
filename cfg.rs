f74bc08f68d59d261e407d9219bc36cfd672902c
Felix S. Klock II <pnkfelix@pnkfx.org>
2014-03-11 11:25:51 +0100

    Got dot rendering working plausible, for seeing graph structure.
    Extended example to include slightly more interesting example.
    
    Still am not seeing any label content on the edges.  Need to check if
    that is a bug in my rendering or if the two simple expressions I am
    using just do not have interesting data on their edges.

--- cfg.rs
+++ cfg.rs
@@ -20,6 +20,10 @@ use rustc::middle;
 use rustc::util::nodemap;
 use cfg::easy_syntax;
 use cfg::easy_syntax::{QuoteCtxt, SyntaxToStr};
+use cfg::graphviz;
+
+use N  = self::middle::cfg::CFGNode;
+use E  = self::middle::cfg::CFGEdge;
 
 mod cfg {
     pub mod easy_syntax;
@@ -37,36 +41,108 @@ fn mk_ast_map() -> ast_map::Map {
     unsafe { cast::transmute(map) }
 }
 
+struct NamedExpr {
+    name: ~str,
+    expr: @syntax::ast::Expr
+}
+
 fn main() {
-    let e = quote_expr!((), { x });
-    println!("expr pre-numbering: {:s}", e.stx_to_str());
-    println!("expr pre-numbering rep: {:?}", e);
+    let e = NamedExpr{ name: ~"just_x",
+                       expr: quote_expr!((), { x }) };
+    process_expr(e);
+    let e = NamedExpr{ name: ~"x_t_y_e_z",
+                       expr: quote_expr!((), { if x { y } else { z } }) };
+    process_expr(e);
+}
+
+fn process_expr(e: NamedExpr) {
+    println!("expr pre-numbering: {:s}", e.expr.stx_to_str());
+    println!("expr pre-numbering rep: {:?}", e.expr);
 
     let (add_node_ids, tcx) = easy_syntax::mk_context();
 
     let map = mk_ast_map();
-    let mut ctx = ast_map::Ctx {
-        map: &map,
-        parent: ast::CRATE_NODE_ID,
-        fold_ops: add_node_ids
-    };
+    let e = {
+        let mut ctx = ast_map::Ctx {
+            map: &map,
+            parent: ast::CRATE_NODE_ID,
+            fold_ops: add_node_ids
+        };
 
-    let e = ctx.fold_expr(e);
-    println!("expr postnumbering: {:s}", e.stx_to_str());
+        NamedExpr{ expr: ctx.fold_expr(e.expr), ..e }
+    };
+    println!("expr postnumbering: {:s}", e.expr.stx_to_str());
     println!("expr postnumbering rep: {:?}", e);
 
     let method_map = @RefCell::new(nodemap::NodeMap::new());
 
-    match e.node {
+    match e.expr.node {
         ast::ExprBlock(b) => {
             let cfg = middle::cfg::CFG::new(tcx, method_map, b);
             println!("cfg: {:?}", cfg);
-            let path = Path::new("/tmp/gviz.dot");
+            let path = Path::new(e.name.as_slice() + ".dot");
             let mut file = File::open_mode(&path, io::Truncate, io::Write);
-            cfg::graphviz::render(&cfg, &mut file);
+            let lcfg = LabelledCFG(e.name, cfg);
+            match cfg::graphviz::render(&map, & &lcfg, &mut file) {
+                Ok(()) => println!("rendered to {}", path.display()),
+                Err(err) => fail!("render failed {}", err)
+            }
         }
         _ => fail!("quoted input for cfg test must \
                               be a expression block { ... }")
     }
 }
 
+struct LabelledCFG {
+    label: ~str,
+    cfg: middle::cfg::CFG,
+}
+
+fn LabelledCFG(label: ~str, cfg: middle::cfg::CFG) -> LabelledCFG {
+    LabelledCFG{ label: label, cfg: cfg }
+}
+
+impl<'a> graphviz::Label<ast_map::Map> for &'a LabelledCFG {
+    fn name(&self, _c: &ast_map::Map) -> ~str {
+        format!("\"{:s}\"", self.label.escape_default())
+    }
+}
+
+impl graphviz::Label<ast_map::Map> for N {
+    fn name(&self, _c: &ast_map::Map) -> ~str {
+        format!("N{}", self.data.id)
+    }
+    fn text(&self, c: &ast_map::Map) -> ~str {
+        c.node_to_str(self.data.id)
+    }
+}
+
+impl graphviz::Label<ast_map::Map> for E {
+    fn name(&self, _c: &ast_map::Map) -> ~str {
+        format!("E")
+    }
+    fn text(&self, c: &ast_map::Map) -> ~str {
+        let mut label = ~"";
+        let mut put_one = false;
+        for &node_id in self.data.exiting_scopes.iter() {
+            if put_one {
+                label = label + ", "
+            } else {
+                put_one = true;
+            }
+            label = label + format!("{}", c.node_to_str(node_id));
+        }
+        label
+    }
+}
+
+impl<'a> graphviz::GraphWalk<'a, N, E> for &'a LabelledCFG {
+    fn nodes(&self) -> ~[&'a N] { self.cfg.graph.all_nodes().iter().collect() }
+    fn edges(&self) -> ~[&'a E] { self.cfg.graph.all_edges().iter().collect() }
+    fn source(&self, edge:&'a E) -> &'a N {
+        self.cfg.graph.node(edge.source())
+    }
+    fn target(&self, edge:&'a E) -> &'a N {
+        self.cfg.graph.node(edge.target())
+    }
+}
--- cfg/graphviz.rs
+++ cfg/graphviz.rs
@@ -1,20 +1,30 @@
 use std::io;
 
-trait Label {
-    fn name<'a>(&'a self) -> ~str;
+pub trait Label<Ctxt> {
+    // short identifier
+    fn name(&self, &Ctxt) -> ~str;
+
+    // descriptive text
+    fn text(&self, c:&Ctxt) -> ~str {
+        self.name(c)
+    }
 }
 
 // All of the type parameters should be associated items. :(
-trait GraphWalk<'a, N, E> {
+pub trait GraphWalk<'a, N, E> {
     fn nodes(&self) -> ~[&'a N];
     fn edges(&self) -> ~[&'a E];
     fn source(&self, edge:&'a E) -> &'a N;
     fn target(&self, edge:&'a E) -> &'a N;
 }
 
-type RenderResult = Result<(), io::IoError>;
+pub type RenderResult = Result<(), io::IoError>;
 
-pub fn render<'a,N:Label,E:Label,G:Label+GraphWalk<'a,N,E>,W:Writer>(g:&G, w:&mut W) -> RenderResult {
+pub fn render<'a,C,N:Label<C>,E:Label<C>,G:Label<C>+GraphWalk<'a,N,E>,W:Writer>(
+    c: &C,
+    g: &G,
+    w: &mut W) -> RenderResult
+{
     let writeln = |w: &mut W, arg:&[&str]| -> RenderResult {
         for &s in arg.iter() {
             try!(w.write_str(s));
@@ -23,17 +33,19 @@ pub fn render<'a,N:Label,E:Label,G:Label+GraphWalk<'a,N,E>,W:Writer>(g:&G, w:&mu
         Ok(())
     };
 
-    try!(writeln(w, ["digraph ", g.name().as_slice(), " {"]));
+    try!(writeln(w, ["digraph ", g.name(c).as_slice(), " {"]));
     for &n in g.nodes().iter() {
         try!(w.write_str("    "));
-        try!(writeln(w, [n.name().as_slice(), ";"]));
+        try!(writeln(w, [n.name(c).as_slice(),
+                         "[label=\"", n.text(c).escape_default().as_slice(), "\"]",
+                         ";"]));
     }
 
     let edges : ~[&'a E] = g.edges();
     for &e in edges.iter() {
-        let escaped_label = e.name().escape_default();
+        let escaped_label = e.text(c).escape_default();
         try!(w.write_str("    "));
-        try!(writeln(w, [g.source(e).name().as_slice(), " -> ", g.target(e).name().as_slice(),
+        try!(writeln(w, [g.source(e).name(c).as_slice(), " -> ", g.target(e).name(c).as_slice(),
                          "[label=\"", escaped_label.as_slice(), "\"];"].as_slice()));
     }
 
