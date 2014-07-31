93364932cdca0c5ef75bb86585c598058e619c69
Felix S. Klock II <pnkfelix@pnkfx.org>
2014-08-01 00:00:58 +0200

    TrackMatchMode: arms are now treated as separate-but-equal (in end effect, that is).
    This is to handle a case like:
    
        match s {
            Err(_) => s,
            Ok(content) => x(content),
        }
    
    which moves on the `Ok` arm pattern but not on the `Err` arm pattern,
    and this is okay because it ends up moving the `s` on both *paths*
    (eventually).

--- src/librustc/middle/expr_use_visitor.rs
+++ src/librustc/middle/expr_use_visitor.rs
@@ -412,11 +412,9 @@ impl<'d,'t,TYPER:mc::Typer> ExprUseVisitor<'d,'t,TYPER> {
                 // walking the arms:
                 self.walk_expr(&**discr);
                 let discr_cmt = return_if_err!(self.mc.cat_expr(&**discr));
-                let mut mode = Unknown;
                 for arm in arms.iter() {
+                    let mut mode = Unknown;
                     self.walk_arm_prepass(discr_cmt.clone(), &**arm, &mut mode);
-                }
-                for arm in arms.iter() {
                     self.walk_arm(discr_cmt.clone(), &**arm, &mode);
                 }
             }
