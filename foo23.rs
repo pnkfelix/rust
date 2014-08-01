9338b2204932edfcb7c1245e161930cddca2ac89
Felix S. Klock II <pnkfelix@pnkfx.org>
2014-08-01 11:55:07 +0200

    root testsuite: made some quick Gc<T> tests.
    These worked out of the box, which I guess is not surprising, except
    that I had thought that these were the cause of our many warnings when
    I had previously attempted to bootstrap libsyntax.

---
+++ foo23.rs
@@ -0,0 +1,35 @@
+#![feature(lang_items)]
+#![no_std]
+#![crate_type="lib"]
+
+#[lang="copy"]  pub trait Copy { }
+#[lang="sized"] pub trait Sized { }
+
+mod marker {
+    #[lang="no_send_bound"]
+    pub struct NoSend;
+}
+
+#[lang="gc"]
+pub struct Gc<T> {
+    _ptr: *mut T,
+    marker: marker::NoSend,
+}
+
+pub fn foo<T:Copy>(b: bool, c: || -> Gc<T>, f: |Gc<T>| -> int) -> int {
+    let x = c();
+    //                                                          // NEEDS_DROP={}
+    let ret = if b {
+    //                                                          // NEEDS_DROP={}
+        f(x) // Variable x copied in this branch ...
+    //                                                          // NEEDS_DROP={}
+    } else {
+    //                                                          // NEEDS_DROP={}
+        3    // ... but not this one ...
+    //                                                          // NEEDS_DROP={}
+    }; // ... but since it is Gc<T>, needs-drop = {}.
+    //        (Gc should not impose drop obligation)
+    //                                                          // NEEDS_DROP={}
+    c();
+    ret
+}
