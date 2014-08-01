0aaa14d6fabb0004219cf116ed46b436e90dc534
Felix S. Klock II <pnkfelix@pnkfx.org>
2014-08-01 07:18:48 +0200

    root testsuite: Update foo19 to reflect separate-but-equal arms.

--- foo19.rs
+++ foo19.rs
@@ -7,14 +7,15 @@
 
 pub enum Pairy<X,Y,Z> { Two(X,Y), One(Z), None }
 
-pub fn foo<A,B:Copy>(c: || -> Pairy<A,bool,B>,
+pub fn foo<A,B:Copy>(c: || -> Pairy<(A,A),bool,B>,
                      dA: |A| -> i8,
                      dB: |B| -> i8) -> i8 {
     let s = c();
     match s {
-        Two(a, true) => dA(a),
+        Two((a1,a2), true) => dA(a1) + dA(a2),
 
-        Two(_, false) => 4, // this is an example of `_` being an
+        Two((_,a2), false) => dA(a2),
+                            // this is an example of `_` being an
                             // "autodrop", since user could not write
                             // the drop themselves except by binding
                             // it to name, which would be a misfeature
