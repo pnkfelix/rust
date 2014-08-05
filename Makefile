06da99078e434a5d9937eb6d87592a2e8fedd9fd
Felix S. Klock II <pnkfelix@pnkfx.org>
2014-08-05 19:25:22 +0200

    root testsuite: still hacking but this is now servicable I think.

--- Makefile
+++ Makefile
@@ -5,33 +5,35 @@ all: $(patsubst %.rs,%.dot,$(FILES))
 
 RUSTC_LIB=$(RUSTC) --crate-type=lib
 
-RUSTC ?= objdir-dbg/x86_64-apple-darwin/stage2/rustc
+STAGE=stage2
+
+RUSTC ?= objdir-dbg/x86_64-apple-darwin/$(STAGE)/rustc
 
 rwildcard=$(foreach d,$(wildcard $1*),$(call rwildcard,$d/,$2) \
   $(filter $(subst *,%,$2),$d))
 
-objdir-dbg/x86_64-apple-darwin/stage2/rustc: src/etc/rustc-wrapper.macosx.sh objdir-dbg/x86_64-apple-darwin/stage2/bin/rustc Makefile $(call rwildcard,src/,*.rs)
-	cd objdir-dbg && make-rustc-stage2 --no-libs
+objdir-dbg/x86_64-apple-darwin/$(STAGE)/rustc: src/etc/rustc-wrapper.macosx.sh objdir-dbg/x86_64-apple-darwin/$(STAGE)/bin/rustc Makefile $(call rwildcard,src/,*.rs)
+	cd objdir-dbg && make-rustc-$(STAGE)
 	cp $< $@
 	chmod +x $@
 
 RUST_LOG=rustc::middle::borrowck,rustc::middle::ty,rustc::middle::typeck,rustc::middle::expr_use_visitor,rustc::middle::region,rustc::middle::trans
 
-%.dylib: %.rs Makefile objdir-dbg/x86_64-apple-darwin/stage2/rustc
+%.dylib: %.rs Makefile objdir-dbg/x86_64-apple-darwin/$(STAGE)/rustc
 	$(RUSTC) $<
 
-%.bin: %.rs Makefile objdir-dbg/x86_64-apple-darwin/stage2/rustc
+%.bin: %.rs Makefile objdir-dbg/x86_64-apple-darwin/$(STAGE)/rustc
 	$(RUSTC) $< -L. -o $@
 
 test.bin: plugin.dylib
 
 test_gensym.bin: gensym.dylib
 
-%.dot: %.rs Makefile objdir-dbg/x86_64-apple-darwin/stage2/rustc
+%.dot: %.rs Makefile objdir-dbg/x86_64-apple-darwin/$(STAGE)/rustc
 	$(RUSTC) -Z flowgraph-print-all --pretty flowgraph=% $< -o $@
 
-%.pp: %.rs Makefile objdir-dbg/x86_64-apple-darwin/stage2/rustc
+%.pp: %.rs Makefile objdir-dbg/x86_64-apple-darwin/$(STAGE)/rustc
 	$(RUSTC)                        --pretty expanded,identified $< -o $@
 
-%.log: %.rs Makefile objdir-dbg/x86_64-apple-darwin/stage2/rustc
+%.log: %.rs Makefile objdir-dbg/x86_64-apple-darwin/$(STAGE)/rustc
 	RUST_LOG=$(RUST_LOG) RUST_BACKTRACE=1 $(RUSTC_LIB) $< 2> $@
--- plugin.rs
+++ plugin.rs
@@ -1,5 +1,5 @@
 #![crate_type="dylib"]
-#![feature(plugin_registrar, quote)]
+#![feature(plugin_registrar, quote, globs)]
 
 // Taken from issue #15750
 
@@ -10,8 +10,16 @@ use rustc::plugin::Registry;
 
 use syntax::codemap::Span;
 use syntax::ext::base::{ExtCtxt, MacResult, MacExpr};
+use syntax::ext::base;
+use syntax::ext::quote;
+use syntax::ext::build::AstBuilder;
+use syntax::parse::token::*;
+use syntax::parse::token;
 use syntax::parse;
 use syntax::ast;
+
+use std::gc::Gc;
+
 
 fn expand(cx: &mut ExtCtxt, _span: Span, tts: &[ast::TokenTree])
           -> Box<MacResult>
@@ -19,17 +27,17 @@ fn expand(cx: &mut ExtCtxt, _span: Span, tts: &[ast::TokenTree])
     // Parse an expression and emit it unchanged.
     println!("expand tts: {}", tts);
     let mut parser = parse::new_parser_from_tts(
-        cx.parse_sess(), cx.cfg(), Vec::from_slice(tts));
+        cx.parse_sess(),
+        cx.cfg(),
+        Vec::from_slice(tts));
 
     let expr = parser.parse_expr();
     println!("expand expr: {}", expr);
 
-    let manual_quote_expr_result = {
-        // syntax::ext::quote::expand_quote_expr(cx, _span, tts)
-        let expanded = syntax::ext::quote::expand_parse_call(
-            cx, _span, "parse_expr", Vec::new(), tts);
-        MacExpr::new(expanded)
-    };
+    let expanded = expand_parse_call(cx, _span, "parse_expr", Vec::new(), tts);
+    println!("built expanded: {}", expanded);
+    let manual_quote_expr_result = MacExpr::new(expanded);
+    println!("built manual_quote_expr_result"); //, manual_quote_expr_result);
     let manual_quote_expr_result_expr = manual_quote_expr_result.make_expr();
     println!("manual_quote_expr_result: {}", manual_quote_expr_result_expr);
 
@@ -39,4 +47,30 @@ fn expand(cx: &mut ExtCtxt, _span: Span, tts: &[ast::TokenTree])
 #[plugin_registrar]
 pub fn plugin_registrar(reg: &mut Registry) {
     reg.register_macro("mymacro", expand);
+}
+
+pub fn expand_parse_call(cx: &ExtCtxt,
+                     sp: Span,
+                     parse_method: &str,
+                     arg_exprs: Vec<Gc<ast::Expr>>,
+                     tts: &[ast::TokenTree]) -> Gc<ast::Expr> {
+    let (cx_expr, tts_expr) = quote::expand_tts(cx, sp, tts);
+
+    let cfg_call = || cx.expr_method_call(
+        sp, cx.expr_ident(sp, quote::id_ext("ext_cx")),
+        quote::id_ext("cfg"), Vec::new());
+
+    let parse_sess_call = || cx.expr_method_call(
+        sp, cx.expr_ident(sp, quote::id_ext("ext_cx")),
+        quote::id_ext("parse_sess"), Vec::new());
+
+    let new_parser_call =
+        cx.expr_call(sp,
+                     cx.expr_ident(sp, quote::id_ext("new_parser_from_tts")),
+                     vec!(parse_sess_call(), cfg_call(), tts_expr));
+
+    let expr = cx.expr_method_call(sp, new_parser_call, quote::id_ext(parse_method),
+                                   arg_exprs);
+
+    quote::expand_wrapper(cx, sp, cx_expr, expr)
 }
