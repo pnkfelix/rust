fa716aa127ad318911dd28971f4576ab8b0216aa
Felix S. Klock II <pnkfelix@pnkfx.org>
2015-01-13 14:37:47 +0100

    NO CHECKIN NOR PUSH: disable coerce-match tests.

--- src/test/run-pass/coerce-match-calls.rs
+++ src/test/run-pass/coerce-match-calls.rs
@@ -8,6 +8,8 @@
 // option. This file may not be copied, modified, or distributed
 // except according to those terms.
 
+// ignore-test
+
 // Check that coercions are propagated through match and if expressions.
 
 use std::boxed::Box;
