d330fe252f057b4062bd2cc88e73aeffecf9e570
Felix S. Klock II <pnkfelix@pnkfx.org>
2015-03-12 16:44:31 +0100

    checkpointing status; but note i should switch all of this over to something more like what I did in the gist for my internals post, namely:
      https://gist.github.com/pnkfelix/4db827b355c737a93576

--- src/test/run-pass/checked-int-ops-1.rs
+++ src/test/run-pass/checked-int-ops-1.rs
@@ -8,11 +8,101 @@
 // option. This file may not be copied, modified, or distributed
 // except according to those terms.
 
+#![feature(checked_arith_ops)]
+#![feature(checked_bit_ops)]
+
 // Tests for `checked_*` methods from `core::num::Int` trait.
 
 use std::fmt;
 use std::num::Int;
 use std::ops::Neg;
+
+#[derive(PartialEq)]
+enum IntType {
+    I8   =  0b1,
+    U8   =  0b10,
+    I16  =  0b100,
+    U16  =  0b1000,
+    I32  =  0b10000,
+    U32  =  0b100000,
+    I64  =  0b1000000,
+    U64  =  0b10000000,
+    ISIZE = 0b100000000,
+    USIZE = 0b1000000000,
+}
+
+const ALL_INT_TYPES: &'static [IntType] = {
+    use self::IntType::*;
+    &[I8, U8, I16, U16, I32, U32, I64, U64, ISIZE, USIZE]
+};
+
+trait BlindAs {
+    fn as_i8(&self) -> i8;
+    fn as_i16(&self) -> i16;
+    fn as_i32(&self) -> i32;
+    fn as_i64(&self) -> i64;
+    fn as_isize(&self) -> isize;
+    fn as_u8(&self) -> u8;
+    fn as_u16(&self) -> u16;
+    fn as_u32(&self) -> u32;
+    fn as_u64(&self) -> u64;
+    fn as_usize(&self) -> usize;
+}
+
+macro_rules! impl_blind_as {
+    ($($t:ty)*) => { $(
+        impl BlindAs for $t {
+            fn as_i8(&self) -> i8 { *self as i8 }
+            fn as_i16(&self) -> i16 { *self as i16 }
+            fn as_i32(&self) -> i32 { *self as i32 }
+            fn as_i64(&self) -> i64 { *self as i64 }
+            fn as_isize(&self) -> isize { *self as isize }
+            fn as_u8(&self) -> u8 { *self as u8 }
+            fn as_u16(&self) -> u16 { *self as u16 }
+            fn as_u32(&self) -> u32 { *self as u32 }
+            fn as_u64(&self) -> u64 { *self as u64 }
+            fn as_usize(&self) -> usize { *self as usize }
+        } )*
+    }
+}
+
+impl_blind_as!(i8 i16 i32 i64 isize u8 u16 u32 u64 usize);
+
+impl IntType {
+    fn checked_is_none<T: Int+fmt::Debug>(&self, n: T) {
+        use self::IntType::*;
+        match *self {
+            I8 => assert_eq!(n.checked_as_i8(), None),
+            U8 => assert_eq!(n.checked_as_u8(), None),
+            I16 => assert_eq!(n.checked_as_i16(), None),
+            U16 => assert_eq!(n.checked_as_u16(), None),
+            I32 => assert_eq!(n.checked_as_i16(), None),
+            U32 => assert_eq!(n.checked_as_u16(), None),
+            I64 => assert_eq!(n.checked_as_i64(), None),
+            U64 => assert_eq!(n.checked_as_u64(), None),
+            ISIZE => assert_eq!(n.checked_as_isize(), None),
+            USIZE => assert_eq!(n.checked_as_usize(), None),
+        }
+    }
+
+    fn checked_matches_blind<T: Int+fmt::Debug+BlindAs>(&self, n: T) {
+        use self::IntType::*;
+        println!("{:?}", n);
+        match *self {
+            I8 => assert_eq!(n.checked_as_i8(), Some(n.as_i8())),
+            U8 => assert_eq!(n.checked_as_u8(), Some(n.as_u8())),
+            I16 => assert_eq!(n.checked_as_i16(), Some(n.as_i16())),
+            U16 => assert_eq!(n.checked_as_u16(), Some(n.as_u16())),
+            I32 => assert_eq!(n.checked_as_i32(), Some(n.as_i32())),
+            U32 => assert_eq!(n.checked_as_u32(), Some(n.as_u32())),
+            I64 => assert_eq!(n.checked_as_i64(), Some(n.as_i64())),
+            U64 => assert_eq!(n.checked_as_u64(), Some(n.as_u64())),
+            ISIZE => assert_eq!(n.checked_as_isize(), Some(n.as_isize())),
+            USIZE => assert_eq!(n.checked_as_usize(), Some(n.as_usize())),
+        }
+    }
+}
+
 
 fn main() {
     #![allow(overflowing_literals)]
@@ -76,6 +166,29 @@ fn main() {
         }
     }
 
+    fn run_neg_tests<T:Int+fmt::Debug>(inputs: &[T], expect: &[Option<T>]) {
+        assert_eq!(inputs.len(), expect.len()); // confirm test is well-formed
+
+        for (i, (&n, &r)) in inputs.iter().zip(expect.iter()).enumerate() {
+            assert_eq!((i, n.checked_neg()), (i, r));
+        }
+    }
+
+
+    type CanConvertViaAs<'a> = &'a [IntType];
+
+    fn run_as_tests<T:Int+fmt::Debug+BlindAs>(inputs: &[(T, &[IntType])]) {
+        for &(n, n_can) in inputs.iter() {
+            for t in ALL_INT_TYPES {
+                if n_can.iter().any(|c| c == t) {
+                    t.checked_matches_blind(n);
+                } else {
+                    t.checked_is_none(n);
+                }
+            }
+        }
+    }
+
     {
         const NUMS: Inputs<i8> = &[0x00, 0x3F,
                                    0x40, 0x7F,
@@ -115,6 +228,12 @@ fn main() {
         run_add_sub_tests(NUMS, EXPECT_ADD);
         run_mul_tests(NUMS, EXPECT_MUL);
         run_div_tests(NUMS, EXPECT_DIV);
+
+        run_neg_tests(&[     0,        1,      -1,  std::i8::MAX, std::i8::MIN],
+                      &[Some(0), Some(-1), Some(1), Some(-0x7F),          None]);
+
+        const INPUTS_AS: Inputs<i8> = &[0, 0x1, 0x7F, 0x80, 0xFF];
+
     }
 
     {
@@ -157,6 +276,10 @@ fn main() {
         run_mul_tests(&NUMS, EXPECT_MUL);
         run_div_tests(&NUMS, EXPECT_DIV);
 
+        run_neg_tests(&[     0,        1,      -1,  std::i16::MAX, std::i16::MIN],
+                      &[Some(0), Some(-1), Some(1), Some(-0x7FFF),          None]);
+
+        const INPUTS_AS: Inputs<i16> = &[0, 0x1, 0x7F, 0x80, 0xFF, 0x100, 0x7FFF, 0x8000, 0xFFFF];
     }
 
     {
@@ -198,6 +321,12 @@ fn main() {
         run_add_sub_tests(NUMS, EXPECT_ADD);
         run_mul_tests(NUMS, EXPECT_MUL);
         run_div_tests(NUMS, EXPECT_DIV);
+
+        run_neg_tests(&[     0,        1,      -1,  std::i32::MAX,      std::i32::MIN],
+                      &[Some(0), Some(-1), Some(1), Some(-0x7FFF_FFFF),          None]);
+
+        const INPUTS_AS: Inputs<i32> = &[0, 0x1, 0x7F, 0x80, 0xFF, 0x100, 0x7FFF, 0x8000, 0xFFFF,
+                                         0x1_0000, 0x7FFF_FFFF, 0x8000_0000, 0xFFFF_FFFF];
     }
 
     {
@@ -242,6 +371,89 @@ fn main() {
         run_add_sub_tests(NUMS, EXPECT_ADD);
         run_mul_tests(NUMS, EXPECT_MUL);
         run_div_tests(NUMS, EXPECT_DIV);
+
+        run_neg_tests(&[     0,        1,      -1,
+                             std::i64::MAX,      std::i64::MIN],
+
+                      &[Some(0), Some(-1), Some(1),
+                        Some(-0x7FFF_FFFF_FFFF_FFFF),     None]);
+
+        const INPUTS_AS: Inputs<i64> =
+            &[0,
+              0x1,           0x7F,                  0x80,                  0xFF,
+              0x100,         0x7FFF,                0x8000,                0xFFFF,
+              0x1_0000,      0x7FFF_FFFF,           0x8000_0000,           0xFFFF_FFFF,
+              0x1_0000_0000, 0x7FFF_FFFF_FFFF_FFFF, 0x8000_0000_0000_0000, 0xFFFF_FFFF_FFFF_FFFF];
+
+
+        const GTE_8: &'static [IntType] = {
+            use self::IntType::*;
+            &[I8, U8, I16, U16, I32, U32, I64, U64, ISIZE, USIZE] };
+        const GTE_16: &'static [IntType] = {
+            use self::IntType::*;
+            &[        I16, U16, I32, U32, I64, U64, ISIZE, USIZE] };
+        const GTE_32: &'static [IntType] = {
+            use self::IntType::*;
+            &[                  I32, U32, I64, U64, ISIZE, USIZE] };
+        #[cfg(target_pointer_width = "32")]
+        const GTE_64: &'static [IntType] = {
+            use self::IntType::*;
+            &[                            I64, U64              ] };
+        #[cfg(target_pointer_width = "64")]
+        const GTE_64: &'static [IntType] = {
+            use self::IntType::*;
+            &[                            I64, U64, ISIZE, USIZE] };
+
+        match true {
+            #[cfg(target_pointer_width = "64")]
+            true => {
+                use self::IntType::*;
+
+                run_as_tests(
+                    &[(   0, &[I8, U8, I16, U16, I32, U32, I64, U64, ISIZE, USIZE]),
+                      (0x01, &[I8, U8, I16, U16, I32, U32, I64, U64, ISIZE, USIZE]),
+                      (0x7f, &[I8, U8, I16, U16, I32, U32, I64, U64, ISIZE, USIZE]),
+                      (0x80, &[    U8, I16, U16, I32, U32, I64, U64, ISIZE, USIZE]),
+                      (0xFF, &[    U8, I16, U16, I32, U32, I64, U64, ISIZE, USIZE]),
+                      (0x100, &[       I16, U16, I32, U32, I64, U64, ISIZE, USIZE]),
+                      (0x7FFF, &[      I16, U16, I32, U32, I64, U64, ISIZE, USIZE]),
+                      (0x8000, &[           U16, I32, U32, I64, U64, ISIZE, USIZE]),
+                      (0xFFFF, &[           U16, I32, U32, I64, U64, ISIZE, USIZE]),
+                      (0x1_0000, &[              I32, U32, I64, U64, ISIZE, USIZE]),
+                      (0x7FFF_FFFF, &[           I32, U32, I64, U64, ISIZE, USIZE]),
+                      (0x8000_0000, &[                U32, I64, U64, ISIZE, USIZE]),
+                      (0xFFFF_FFFF, &[                U32, I64, U64, ISIZE, USIZE]),
+                      (0x1_0000_0000, &[                   I64, U64, ISIZE, USIZE]),
+                      (0x7FFF_FFFF_FFFF_FFFF, &[           I64, U64, ISIZE, USIZE]),
+                      (0x8000_0000_0000_0000, &[           I64, U64, ISIZE, USIZE]),
+                      (0xFFFF_FFFF_FFFF_FFFF, &[           I64, U64, ISIZE, USIZE])]);
+            }
+            #[cfg(target_pointer_width = "32")]
+            true => {
+                use self::IntType::*;
+
+                run_as_tests(
+                    &[(   0, &[I8, U8, I16, U16, I32, U32, I64, U64, ISIZE, USIZE]),
+                      (0x01, &[I8, U8, I16, U16, I32, U32, I64, U64, ISIZE, USIZE]),
+                      (0x7f, &[I8, U8, I16, U16, I32, U32, I64, U64, ISIZE, USIZE]),
+                      (0x80, &[    U8, I16, U16, I32, U32, I64, U64, ISIZE, USIZE]),
+                      (0xFF, &[    U8, I16, U16, I32, U32, I64, U64, ISIZE, USIZE]),
+                      (0x100, &[       I16, U16, I32, U32, I64, U64, ISIZE, USIZE]),
+                      (0x7FFF, &[      I16, U16, I32, U32, I64, U64, ISIZE, USIZE]),
+                      (0x8000, &[           U16, I32, U32, I64, U64, ISIZE, USIZE]),
+                      (0xFFFF, &[           U16, I32, U32, I64, U64, ISIZE, USIZE]),
+                      (0x1_0000, &[              I32, U32, I64, U64, ISIZE, USIZE]),
+                      (0x7FFF_FFFF, &[           I32, U32, I64, U64, ISIZE, USIZE]),
+                      (0x8000_0000, &[                U32, I64, U64, ISIZE, USIZE]),
+                      (0xFFFF_FFFF, &[                U32, I64, U64, ISIZE, USIZE]),
+                      (0x1_0000_0000, &[                   I64, U64 ]),
+                      (0x7FFF_FFFF_FFFF_FFFF, &[           I64, U64 ]),
+                      (0x8000_0000_0000_0000, &[           I64, U64 ]),
+                      (0xFFFF_FFFF_FFFF_FFFF, &[           I64, U64 ])]);
+            }
+
+            _ => panic!("need case for other target_pointer_width"),
+        }
     }
 
     {
@@ -330,5 +542,18 @@ fn main() {
         run_add_sub_tests(NUMS, EXPECT_ADD);
         run_mul_tests(NUMS, EXPECT_MUL);
         run_div_tests(NUMS, EXPECT_DIV);
+
+        const INPUTS_NEG: Inputs<isize> =
+            &[     0,        1,      -1,               std::isize::MAX, std::isize::MIN];
+
+        #[cfg(target_pointer_width = "32")]
+        const EXPECT_NEG: &'static [Option<isize>] =
+            &[Some(0), Some(-1), Some(1), Some(-0x7FFF_FFFF),            None];
+
+        #[cfg(target_pointer_width = "64")]
+        const EXPECT_NEG: &'static [Option<isize>] =
+            &[Some(0), Some(-1), Some(1), Some(-0x7FFF_FFFF_FFFF_FFFF),            None];
+
+        run_neg_tests(INPUTS_NEG, EXPECT_NEG);
     }
 }
