d29e7c0adfd2f0f0b5ef37e13075d83cf5eb7129
Felix S. Klock II <pnkfelix@pnkfx.org>
2016-03-31 18:47:14 +0200

    Some notes on an attribute that I will probably add eventually but do not have yet.

--- src/libsyntax/feature_gate.rs
+++ src/libsyntax/feature_gate.rs
@@ -259,6 +259,16 @@ const KNOWN_FEATURES: &'static [(&'static str, &'static str, Option<u32>, Status
 
     // pub(restricted) visibilities (RFC 1422)
     ("pub_restricted", "1.9.0", Some(32409), Active),
+
+    // Allows using the patchpoint_fn attribute, which indicates that
+    // calls/invocations of such a function/method may emit a
+    // patchpoint.
+    //
+    // (It will probably emit a patchpoint unconditionally in the
+    // first iteration, but I may later constrain that so that it only
+    // emits a patchpoint if there are a positive number of tracked
+    // values in scope at the call site.)
+    ("patchpoint_fn", "1.7.0", Some(17668), Active),
 ];
 // (changing above list without updating src/doc/reference.md makes @cmr sad)
 
