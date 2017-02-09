aa16f206a7abece516879c6766f7b26e7de75d3a
Felix S. Klock II <pnkfelix@pnkfx.org>
2017-02-09 10:59:30 +0100

    Tests for EndRegion. Uses `-Z identify_regions` so one can eyeball output for sanity.

---
+++ src/test/mir-opt/end_region_1.rs
@@ -0,0 +1,31 @@
+// Copyright 2017 The Rust Project Developers. See the COPYRIGHT
+// file at the top-level directory of this distribution and at
+// http://rust-lang.org/COPYRIGHT.
+//
+// Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
+// http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
+// <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
+// option. This file may not be copied, modified, or distributed
+// except according to those terms.
+
+// compile-flags: -Z identify_regions
+// ignore-tidy-linelength
+
+fn main() {
+    let a = 3;
+    let b = &a;
+}
+
+// END RUST SOURCE
+// START rustc.node4.TypeckMir.before.mir
+//     bb0: {
+//         StorageLive(_1);
+//         _1 = const 3i32;
+//         StorageLive(_2);
+//         _2 = &'14ce _1;
+//         StorageDead(_2);
+//         EndRegion('14ce);
+//         StorageDead(_1);
+//         return;
+//     }
+// END rustc.node4.TypeckMir.before.mir
---
+++ src/test/mir-opt/end_region_3.rs
@@ -0,0 +1,53 @@
+// Copyright 2017 The Rust Project Developers. See the COPYRIGHT
+// file at the top-level directory of this distribution and at
+// http://rust-lang.org/COPYRIGHT.
+//
+// Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
+// http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
+// <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
+// option. This file may not be copied, modified, or distributed
+// except according to those terms.
+
+// compile-flags: -Z identify_regions
+// ignore-tidy-linelength
+
+fn main() {
+    let a = true;
+    loop {
+        let b = &a;
+        if a { break; }
+        let c = &a;
+    }
+}
+
+// END RUST SOURCE
+// START rustc.node4.TypeckMir.before.mir
+//     bb0: {
+//         StorageLive(_1);
+//         _1 = const true;
+//         goto -> bb1;
+//     }
+//     bb1: {
+//         StorageLive(_3);
+//         _3 = &'17ce _1;
+//         StorageLive(_5);
+//         _5 = _1;
+//         StorageDead(_5); // (see issue #38669)
+//         if(_5) -> [true: bb2, false: bb3];
+//     }
+//     bb2: {
+//         StorageDead(_3);
+//         EndRegion('17ce);
+//         StorageDead(_1);
+//         return;
+//     }
+//     bb3: {
+//         StorageLive(_8);
+//         _8 = &'33ce _1;
+//         StorageDead(_8);
+//         EndRegion('33ce);
+//         StorageDead(_3);
+//         EndRegion('17ce);
+//         goto -> bb1;
+//     }
+// END rustc.node4.TypeckMir.before.mir
