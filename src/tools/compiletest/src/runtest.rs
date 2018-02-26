75c0f6e0857a74ddcfaf9b7de1174ce64e26f63a
Felix S. Klock II <pnkfelix@pnkfx.org>
2018-02-26 14:46:43 +0100

    Include the test name when reporting that an expected line was not found in a mir-opt test.

--- src/tools/compiletest/src/runtest.rs
+++ src/tools/compiletest/src/runtest.rs
@@ -2698,10 +2698,12 @@ impl<'test> TestCx<'test> {
             panic!(
                 "Did not find expected line, error: {}\n\
                  Expected Line: {:?}\n\
+                 Test Name: {}\n\
                  Expected:\n{}\n\
                  Actual:\n{}",
                 extra_msg,
                 expected_line,
+                test_name,
                 expected_content,
                 normalize_all
             );
