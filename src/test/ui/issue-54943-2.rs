49bdeb76467ecc144b46d0369e099a0ef2d1f3bb
David Wood <david@davidtw.co>
2018-12-09 03:07:31 +0100

    Fix unresolved inference variable ICE.
    This commit moves well-formedness check for the
    `UserTypeAnnotation::Ty(..)` case from always running to only when the
    code is reachable. This solves the ICE that resulted from
    `src/test/ui/issue-54943-1.rs` (a minimal repro of `dropck-eyepatch`
    run-pass tests that failed).
    
    The main well-formedness check that was intended to be run despite
    unreachable code still is, that being the
    `UserTypeAnnotation::TypeOf(..)` case. Before this PR, the other case
    wasn't being checked at all.
    
    It is possible to fix this ICE while still always checking
    well-formedness for the `UserTypeAnnotation::Ty(..)` case but that
    solution will ICE in unreachable code for that case, the diff for
    that change [can be found here](0).
    
    [0]: https://gist.github.com/davidtwco/f9751ffd9c0508f7251c0f17adc3af53

---
+++ src/test/ui/issue-54943-2.rs
@@ -0,0 +1,28 @@
+// Copyright 2017 The Rust Project Developers. See the COPYRIGHT
+// file at the top-level directory of this distribution and at
+// http://rust-lang.org/COPYRIGHT.
+//
+// Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
+// http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
+// <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
+// option. This file may not be copied, modified, or distributed
+// except according to those terms.
+
+#![feature(nll)]
+
+// This test is a minimal version of an ICE in the dropck-eyepatch tests
+// found in the fix for #54943. In particular, this test is in unreachable
+// code as the initial fix for this ICE only worked if the code was reachable.
+
+// compile-pass
+
+fn foo<T>(_t: T) {
+}
+
+fn main() {
+    return;
+
+    struct A<'a, B: 'a>(&'a B);
+    let (a1, a2): (String, A<_>) = (String::from("auto"), A(&"this"));
+    foo((a1, a2));
+}
