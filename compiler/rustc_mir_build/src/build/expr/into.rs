1fb7f11f2c732db9398560d436fc509a82e16122
Matthew Jasper <mjjasper1@gmail.com>
2024-05-31 16:21:33 +0000

    Use record_operands_moved more in mir building

--- compiler/rustc_mir_build/src/build/expr/into.rs
+++ compiler/rustc_mir_build/src/build/expr/into.rs
@@ -379,6 +379,7 @@ impl<'a, 'tcx> Builder<'a, 'tcx> {
                     user_ty,
                     active_field_index,
                 ));
+                this.record_operands_moved(&fields.raw);
                 this.cfg.push_assign(
                     block,
                     source_info,
@@ -557,6 +558,7 @@ impl<'a, 'tcx> Builder<'a, 'tcx> {
                     )
                 );
                 let resume = this.cfg.start_new_block();
+                this.record_operands_moved(slice::from_ref(&value));
                 this.cfg.terminate(
                     block,
                     source_info,
